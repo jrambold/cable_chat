69970a7ee92bd03a5d61d43a8ceedbd0ba5a1979
jrambold <jrambold@gmail.com>
2018-07-06 11:33:02 -0600

    still not working

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,7 +10,9 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+
 //= require rails-ujs
 //= require activestorage
+//= require cable
 //= require turbolinks
 //= require_tree .
